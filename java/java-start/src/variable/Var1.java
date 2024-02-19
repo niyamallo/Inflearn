f2891ef65ae6aff4bfcdbe8024bb766e2b9e9b1e
김남우 <treenicole@naver.com>
2024-02-20 02:27:03 +0900

    입문2 변수 변수 시작

---
+++ java/java-start/src/variable/Var1.java
@@ -0,0 +1,11 @@
+package variable;
+// 패키지를 만들었다면 패키지에 들어간 자바 파일 첫줄에 소속된 패키지를 선언해야 함.
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+        // 변수가 없으면 10을 20으로 바꾸고싶을 때 모든 부분을 하나하나 수정해야 함.
+    }
+}
---
+++ java/java-start/src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 20; // 변수 초기화. 이 부분을 변경하면 변수 a가 관여하는 모든 부분에 영향을 미칠 수 있다.
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
