c04ca02fe2aec22836823c59d7caa8b12178c879
김남우 <treenicole@naver.com>
2024-02-20 01:52:19 +0900

    입문1 Hello World 주석(Comment)

---
+++ java/java-start/src/CommentJava.java
@@ -0,0 +1,15 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        // psvm + Enter 하면 public static void main 자동생성
+        System.out.println("Hello Java1"); // Hello Java1을 출력합니다.
+        // sout + Enter 하면 System.out.println(); 자동생성
+//        System.out.println("Hello Java2");
+
+        /*
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+        여러 줄 주석처리 하는 법
+         */
+
+    }
+}
