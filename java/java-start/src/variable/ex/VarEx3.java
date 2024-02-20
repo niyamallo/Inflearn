7fd86bf09e77087e13c419047fbc7361d24cb1e8
김남우 <treenicole@naver.com>
2024-02-20 23:11:22 +0900

    입문2 변수 문제와 풀이

---
+++ java/java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        System.out.println(l);
+        System.out.println("l = " + l);
+        // 참고: soutv + Enter 하면 가까운 변수의 이름을 붙여서 출력해줌.
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
