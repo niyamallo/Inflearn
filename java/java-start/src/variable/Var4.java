a13479d62b92aa9ea2c400ae1703c69e14b28faf
김남우 <treenicole@naver.com>
2024-02-20 21:22:14 +0900

    입문2 변수 변수 선언과 초기화

---
+++ java/java-start/src/variable/Var4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        
+        int c, d; // 한 번에 변수 여러개 선언하는 방법
+
+    }
+}
---
+++ java/java-start/src/variable/Var6.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+        // 주석을 풀면 컴파일 에러 발생.
+        // a: variable a might not have been initialized
+        // 클래스 변수와 인스턴스 변수는 자바가 자동으로 초기화를 진행해 주지만,
+        // 지역 변수(Local Variable)는 개발자가 직접 초기화를 해 주어야 함.
+    }
+}
