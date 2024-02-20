0a36d05ac715e486fa7d912d00e1760f5d48a381
김남우 <treenicole@naver.com>
2024-02-21 01:40:16 +0900

    입문3 연산자 문자열 더하기

---
+++ java/java-start/src/operator/Operator2.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        //문자열과 문자열 더하기1
+        String result1 = "hello " + "world";
+        System.out.println(result1);
+
+        //문자열과 문자열 더하기2
+        String s1 = "string1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+
+        //문자열과 숫자 더하기1
+        String result3 = "a + b = " + 10; //문자열 + 숫자 하면 숫자를 문자열로 변환해서 더함.
+        System.out.println(result3);
+
+        //문자열과 숫자 더하기2
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = str + num;
+        System.out.println(result4);
+    }
+}
