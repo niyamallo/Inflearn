cb85d3d0703e460f9f1aca208047408215c74526
김남우 <treenicole@naver.com>
2024-02-28 22:53:36 +0900

    기본2 기본형과 참조형 기본형vs참조형3-메서드 호출

---
+++ java/java-basic/src/ref/MethodChange2.java
@@ -0,0 +1,19 @@
+package ref;
+
+public class MethodChange2 {
+
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        System.out.println("메서드 호출 전: dataA.value = " +dataA.value); //10
+        System.out.println("dataA=" + dataA); //x001
+        changeReference(dataA);
+        System.out.println("메서드 호출 후: dataA.value = " +dataA.value); //20
+    }
+
+    public static void changeReference(Data dataX) {
+        System.out.println("dataX=" + dataX); //x001
+        dataX.value = 20;
+    }
+
+}
