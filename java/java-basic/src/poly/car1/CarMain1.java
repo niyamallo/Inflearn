ddaae8706752afe0ab52e7832ecdadac3c1b9d35
김남우 <treenicole@naver.com>
2024-04-04 01:29:06 +0900

    기본12 다형성과 설계 OCP(Open-Closed Principle) 원칙

--- java/java-basic/src/poly/car1/CarMain1.java
+++ java/java-basic/src/poly/car1/CarMain1.java
@@ -14,5 +14,14 @@ public class CarMain1 {
         Model3Car model3Car = new Model3Car();
         driver.setCar(model3Car);
         driver.drive();
+
+        //OCP(Open-Closed Principle)
+        //기능을 확장해도 main() 일부를 제외한 프로그램의 핵심 부분의 코드는 전혀 수정하지 않음.
+        //확장에는 열려있고(Open)
+        //클라이언트(Driver)의 코드는 수정하지 않도록(Closed)
+        //차량 변경(model3 -> newCar)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
+        driver.drive();
     }
 }
