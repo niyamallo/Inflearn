3d234c42a5ba07a5eddfee9d1c2f8d8b9871ac30
김남우 <treenicole@naver.com>
2024-04-03 23:24:11 +0900

    기본12 다형성-역할과 구현 예제2

--- java/java-basic/src/poly/car0/CarMain0.java
+++ java/java-basic/src/poly/car0/CarMain0.java
@@ -5,8 +5,16 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver driver = new Driver();
         K3Car k3Car = new K3Car();
-
         driver.setK3Car(k3Car);
         driver.drive();
+
+        //추가
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
+        
+        //자동차가 늘어날 때마다 Driver의 코드를 계속 고쳐야하는 문제 발생
+        //역할과 구현을 구분하지 않았기 때문!
     }
 }
