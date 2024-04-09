f7c52f4a6b7eb70af22ea6e0b3fb1205714c9eb3
김남우 <treenicole@naver.com>
2024-04-10 03:58:29 +0900

    스프링입문3_회원 관리 예제-백엔드 개발_회원 리포지토리 테스트케이스 작성

--- spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRespository.java
+++ spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRespository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRespository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
