64f24edd0e3fb851022e7fc6d3136bc72f44562f
김남우 <treenicole@naver.com>
2024-04-11 22:39:11 +0900

    스프링입문4_스프링 빈과 의존관계_자바 코드로 직접 스프린 빈 등록하기

--- spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRespository.java
+++ spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRespository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRespository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
