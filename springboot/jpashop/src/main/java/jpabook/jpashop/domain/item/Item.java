aed4d656ee38e70fc0d790bd2b6859f2373b2688
김남우 <treenicole@naver.com>
2024-04-17 03:38:05 +0900

    실전스프링부트5_상품 도메인 개발_비즈니스 로직 추가

--- springboot/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ springboot/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public class Item {
 
     @Id @GeneratedValue
@@ -25,4 +26,25 @@ public class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+    //데이터가 있는 곳에서 관련 로직을 처리하는게 가장 객체지향적인 방법
+    //setter는 가급적 사용x 이 안에서 필요한 로직을 처리하자.
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
